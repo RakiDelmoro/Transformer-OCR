d321a613b9e1a630140e3b4ee1115bca75aee3d8
root <rakidelmoro@gmail.com>
2024-03-06 11:07:34 +0000

    Update

---
+++ constants.py
@@ -0,0 +1,23 @@
+import torch
+import string
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+IMAGE_SIZE = (32, 800)
+
+NUM_EPOCHS = 100000000
+BATCH_SIZE = 64
+LEARNING_RATE = 1e-5
+
+LETTERS = [ch for ch in string.ascii_letters]
+NUMBERS = [num for num in string.digits]
+SYMBOLS = ["!", "?", ".", ":", ";", "'", "&", " "]
+START_TOKEN = '\N{Start of Text}'
+END_TOKEN = '\N{End of Text}'
+PAD_TOKEN = '\N{Substitute}'
+
+CHARS = ["\x00", PAD_TOKEN, START_TOKEN, END_TOKEN] + LETTERS + NUMBERS + SYMBOLS
+
+MAX_PHRASE_LENGTH = 95
+NUM_PHRASE_LENGTH = 10
+INFERENCE_PHRASE_LENGTH = 5
